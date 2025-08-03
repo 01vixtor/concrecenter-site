7bb23114253512480b8c3a4c0023f1aa1ce34cec
01vixtor <victorns.kkjs@gmail.com>
2025-08-03 12:46:35 -0300

    Delete scripts/contador.js

--- scripts/contador.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  let proj = new CountUp('projetos', 150, { duration: 2 });
-  let exp = new CountUp('experiencia', 20, { duration: 2 });
-  if (!proj.error) proj.start();
-  if (!exp.error) exp.start();
-});
